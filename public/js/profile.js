93809be7a2f6e1a62f700cff374850c38d83e288
mediazjr <mediazjr@gmail.com>
2022-01-10 10:14:15 -0800

    now able to see reviews on /review/:id

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,47 +1,47 @@
-const newFormHandler = async (event) => {
-  event.preventDefault();
+const newFormHandler = async(event) => {
+    event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+    const name = document.querySelector('#review-name').value.trim();
+    const comment = document.querySelector('#review-comment').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create project');
+    if (name && comment) {
+        const response = await fetch(`/api/reviews`, {
+            method: 'POST',
+            body: JSON.stringify({ name, comment }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            document.location.replace('/login');
+        }
     }
-  }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+const delButtonHandler = async(event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
+        const response = await fetch(`/api/reviews/${id}`, {
+            method: 'DELETE',
+        });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to delete review');
+        }
     }
-  }
 };
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+    .querySelector('.new-review-form')
+    .addEventListener('submit', newFormHandler);
 
 document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+    .querySelector('.review-list')
+    .addEventListener('click', delButtonHandler);
